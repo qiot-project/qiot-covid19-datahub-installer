584f9e67428f3abcdbfbecbe23e8968d7c321f12
wpernath <w.pernath@gmx.de>
2021-05-18 19:13:22 +0200

    Added installer to configure vault on POD

--- datahub-install.sh
+++ datahub-install.sh
@@ -9,6 +9,8 @@ declare PRJ_PREFIX="covid19"
 declare COMMAND="help"
 declare TMP="/tmp/datahub"
 
+VAULT_KEY=
+VAULT_TOKEN=
 
 valid_command() {
   local fn=$1; shift
@@ -53,6 +55,8 @@ done
 declare -r project_name=$PRJ_PREFIX-datahub
 declare -r git_pipelines="$TMP/pipelines"
 declare -r git_operators="$TMP/operators"
+declare -r charts="$TMP/charts"
+declare -r ca=$TMP/ca
 
 
 command.help() {
@@ -77,16 +81,77 @@ command.help() {
 EOF
 }
 
+install_certmanager() {
+    info "Installing cert-manager into cert-manager namespace"
+    oc get ns cert-manager 2>/dev/null || {
+        info "Installing cert-manager into cert-manager namespace"
+        oc new-project cert-manager >/dev/null
+    }
+    
+    helm repo add jetstack https://charts.jetstack.io
+    helm repo update
+
+    helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.2.0 --set installCRDs=true
+    oc apply -f $git_operators/cert-manager/sample/test-resource.yaml -n cert-manager
+
+    info "Generating keys..."
+    openssl req -new -nodes -newkey rsa:2048 -x509 -keyout $ca/tls.key -out $ca/tls.crt -days 365 -subj "/CN=qiot-project.github.io" -extensions v3_ca
+    oc create secret generic qiot-ca --from-file=$ca/ -n cert-manager
+
+    info ""
+    oc apply -f $git_operators/sample/issuer-qiot-ca-sample.yaml -n cert-manager
+    oc apply -f $git_operators/sample/certificate-qiot-device-sample.yaml -n cert-manager
+
+    # From here, we have to have a working vault installed
+
+}
+
+install_vault() {
+    info "Installing hashicorp/vault into cert-manager namespace"
+    oc get ns cert-manager 2>/dev/null || {
+        oc new-project cert-manager >/dev/null
+    }
+
+    helm repo add hashicorp https://helm.releases.hashicorp.com
+    helm repo update
+    helm upgrade --install vault hashicorp/vault --namespace cert-manager -f $git_operators/vault/override-standalone.yaml
+
+    info "Configuring hashicorp/vault..."
+    
+    # copy policy.hcl and script to pod
+    cp scripts/configure-vault.sh $git_operators/vault/sample/
+    oc get pod
+    oc rsync $git_operators/vault/sample/ vault-0:/tmp/
+ 
+    # execute that script on pod
+    oc exec vault-0 -- sh /tmp/configure-vault.sh > $TMP/configure-vault.out
+
+    # grep output to get VAULT_KEY and VAULT_TOKEN
+    VAULT_KEY=$(cat $TMP/configure-vault.out | grep "Exported KEY: " | cut -d' ' -f 3)
+    VAULT_TOKEN=$(cat $TMP/configure-vault.out | grep "Exported TOKEN: " | cut -d' ' -f 3)
+
+    [ -z $VAULT_KEY ] && err "Could not determine VAULT_KEY. Exiting. Please rerun again."
+    [ -z $VAULT_TOKEN ] && err "Could not determine VAULT_TOKEN. Exiting. Please rerun again."
+
+    info "Vault successfully installed!"
+    info "VAULT_KEY   = $VAULT_KEY"
+    info "VAULT_TOKEN = $VAULT_TOKEN"
+
+}
+
+
+# Install all the charts and configure the system
 command.install() {
     oc version >/dev/null 2>&1 || err "no oc binary found"
     helm version >/dev/null 2>&1 || err "no helm binary found!"
+    openssl version >/dev/null 2>&1 || err "no openssl binary found!"
 
     kube_context=$(oc whoami -c)
     info "Using $kube_context to install datahub..."
 
     info "Creating namespace for $PRJ_PREFIX datahub..."
     oc get ns $project_name 2>/dev/null  || { 
-        oc new-project $project_name
+        oc new-project $project_name >/dev/null 
         info "$project_name created"
     }
 
@@ -96,19 +161,51 @@ command.install() {
     # delete tmp folders if exists
     [ -d "$git_operators" ] && rm -rf "$git_operators"
     [ -d "$git_pipelines" ] && rm -rf "$git_pipelines"
+    [ -d "$charts" ] && rm -rf "$charts"
+    [ -d "$ca" ] && rm -rf "$ca"
 
     mkdir -p $git_pipelines
     mkdir -p $git_operators
-    git clone https://github.com/qiot-project/qiot-covid19-datahub-operators.git $git_operators
-    git clone https://github.com/qiot-project/qiot-covid19-datahub-pipelines.git $git_pipelines
+    mkdir -p $charts
+    mkdir -p $ca
+    git clone https://github.com/qiot-project/qiot-covid19-datahub-operators.git $git_operators >/dev/null
+    git clone https://github.com/qiot-project/qiot-covid19-datahub-pipelines.git $git_pipelines >/dev/null
     
 
     # First the easy part: Generate the pipelines chart... this is easy, just call a script.
     info "Generating helm chart for pipelines..."
     $git_pipelines/build-chart.sh
+    mv $git_pipelines/target/qiot-covid19-datahub-pipelines*.tgz $charts/
 
+    # Generate all the other charts from $git_operators folder
+    info "Generating Nexus chart..."
+    helm package $git_operators/nexus -u -d $charts
 
+    info "Generating PostgreSQL chart..."
+    helm package $git_operators/postgreSQL -u -d $charts
+
+    info "Generating InfluxDB2 chart..."
+    helm package $git_operators/influxdb2 -u -d $charts
+
+    info "Generating Grafana chart..."
+    helm package $git_operators/Grafana -u -d $charts
+
+    # Now install cert-manager
+    install_vault
+    install_certmanager
 } 
+
+command.uninstall() {
+    oc version >/dev/null 2>&1 || err "no oc binary found"
+    helm version >/dev/null 2>&1 || err "no helm binary found!"
+
+    kube_context=$(oc whoami -c)
+    info "Using $kube_context to uninstall datahub..."
+
+    oc delete project cert-manager
+    oc delete project $project_name
+
+}
 
 
 main() {
---
+++ scripts/configure-vault.sh
@@ -0,0 +1,41 @@
+    #!/bin/bash
+    # This script will be copied to the vault-0 pod after installing
+    # hashicorp/vault 
+    #
+    # It will configure the vault with everything required.
+    # It expeects a policy.hcl file in /tmp on the pod
+    #
+    echo "Configuring vault on pod"
+    vault operator init -tls-skip-verify -key-shares=1 -key-threshold=1 > /tmp/vault.txt
+    export KEYS=$(cat /tmp/vault.txt | head -n 1 | cut -d' ' -f 4)
+    export VAULT_TOKEN=$(cat /tmp/vault.txt | head -n3|tail -n1|cut -d' ' -f4)
+
+    # unseal 
+    vault operator unseal -tls-skip-verify $KEYS
+
+    # enable pki engine
+    vault secrets enable -tls-skip-verify --path=cert-manager-io pki
+    vault secrets tune -tls-skip-verify -max-lease-ttl=8760h cert-manager-io
+    vault write -tls-skip-verify cert-manager-io/root/generate/internal common_name=qiot-project.github.io ttl=8760h
+
+    # CRL configuration
+    vault write -tls-skip-verify cert-manager-io/config/urls issuing_certificates="https://127.0.0.1:8200/v1/cert-manager-io/ca" crl_distribution_points="https://127.0.0.1:8200/v1/cert-manager-io/crl"
+
+    # Configure Domain for qiot-project.github.io
+    vault write -tls-skip-verify cert-manager-io/roles/qiot-project-github-io allowed_domains=qiot-project.github.io,svc allow_subdomains=true allowed_other_sans="*" allow_glob_domains=true max_ttl=72h
+
+    # Enable Kubernetes Auth
+    JWT=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+    KUBERNETES_HOST=https://${KUBERNETES_PORT_443_TCP_ADDR}:443
+
+    vault auth enable --tls-skip-verify kubernetes
+    vault write --tls-skip-verify auth/kubernetes/config token_reviewer_jwt=$JWT kubernetes_host=$KUBERNETES_HOST kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+
+    # Create PKI policy
+    vault policy write --tls-skip-verify pki-policy /tmp/policy.hcl
+
+    # Authorize Cert-manager
+    vault write --tls-skip-verify auth/kubernetes/role/cert-manager bound_service_account_names=cert-manager bound_service_account_namespaces='cert-manager' policies=pki-policy ttl=2h
+
+    echo "Exported KEY: $KEYS"
+    echo "Exported TOKEN: $VAULT_TOKEN"
