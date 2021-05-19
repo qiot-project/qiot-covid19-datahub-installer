8e5f3a5bdcd802197f6c378861e5c4d82f027d84
wpernath <w.pernath@gmx.de>
2021-05-19 14:36:38 +0200

    cert-manager and vault now being automatically installed and configured

--- datahub-install.sh
+++ datahub-install.sh
@@ -8,6 +8,7 @@ declare -r SCRIPT_DIR=$(cd -P $(dirname $0) && pwd)
 declare PRJ_PREFIX="covid19"
 declare COMMAND="help"
 declare TMP="/tmp/datahub"
+declare BASE_DOMAIN="apps-crc.testing"
 
 VAULT_KEY=
 VAULT_TOKEN=
@@ -18,7 +19,7 @@ valid_command() {
 }
 
 info() {
-    printf "\n# INFO: $@\n"
+    printf "# INFO: $@\n"
 }
 
 err() {
@@ -34,6 +35,10 @@ while (( "$#" )); do
       ;;
     -p|--project-prefix)
       PRJ_PREFIX=$2
+      shift 2
+      ;;
+    -d|--base-domain)
+      BASE_DOMAIN=$2
       shift 2
       ;;
     -t|--temporary-dir)
@@ -53,6 +58,10 @@ while (( "$#" )); do
 done
 
 declare -r project_name=$PRJ_PREFIX-datahub
+declare -r dev_proj=$PRJ_PREFIX-dev
+declare -r int_proj=$PRJ_PREFIX-int
+declare -r prod_proj=$PRJ_PREFIX-prod
+
 declare -r git_pipelines="$TMP/pipelines"
 declare -r git_operators="$TMP/operators"
 declare -r charts="$TMP/charts"
@@ -75,8 +84,9 @@ command.help() {
       help                           Help about this command
 
   OPTIONS:
-      -p|--project-prefix [string]   Prefix to be added to datahub project names e.g. PREFIX-datahub (covid19-datahub)
+      -p|--project-prefix [string]   Prefix to be added to datahub project names e.g. PREFIX-datahub (default: $PRJ_PREFIX)
       -t|--temporary-dir [string]    Define the tmp dir for work (default: $TMP)
+      -d|--base-domain [string]      Define the base domain for all routes (default: $BASE_DOMAIN)
 
 EOF
 }
@@ -88,21 +98,50 @@ install_certmanager() {
         oc new-project cert-manager >/dev/null
     }
     
-    helm repo add jetstack https://charts.jetstack.io
-    helm repo update
+    helm repo add jetstack https://charts.jetstack.io > /dev/null
+    helm repo update > /dev/null
 
-    helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.2.0 --set installCRDs=true
-    oc apply -f $git_operators/cert-manager/sample/test-resource.yaml -n cert-manager
+    helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.2.0 --set installCRDs=true > /dev/null
+    sleep 10
 
     info "Generating keys..."
     openssl req -new -nodes -newkey rsa:2048 -x509 -keyout $ca/tls.key -out $ca/tls.crt -days 365 -subj "/CN=qiot-project.github.io" -extensions v3_ca
     oc create secret generic qiot-ca --from-file=$ca/ -n cert-manager
 
-    info ""
-    oc apply -f $git_operators/sample/issuer-qiot-ca-sample.yaml -n cert-manager
-    oc apply -f $git_operators/sample/certificate-qiot-device-sample.yaml -n cert-manager
+    # we need to wait until everything is settled. Otherwise we can't 
+    sleep 20
+
+    oc apply -f $git_operators/cert-manager/sample/issuer-qiot-ca-sample.yaml -n cert-manager
+    oc apply -f $git_operators/cert-manager/sample/certificate-qiot-device-sample.yaml -n cert-manager
 
     # From here, we have to have a working vault installed
+    [ -z $VAULT_KEY ] && err "Could not determine VAULT_KEY. Exiting. Please rerun again."
+    [ -z $VAULT_TOKEN ] && err "Could not determine VAULT_TOKEN. Exiting. Please rerun again."
+
+    oc apply -f $git_operators/cert-manager/sample/issuer-qiot-vault-sample.yaml -n cert-manager
+    oc apply -f $git_operators/cert-manager/sample/certificate-qiot-device-vault-issuer.yaml -n cert-manager
+
+    # get vault address
+    export VAULT_ADDR=https://$(oc get route vault --no-headers -o custom-columns=HOST:.spec.host -n cert-manager)
+    export KEYS=$VAULT_KEY
+
+    # configure all the projects
+    sh $git_operators/cert-manager/covid19/setup.sh $project_name $BASE_DOMAIN
+    sh $git_operators/cert-manager/covid19/setup.sh $dev_proj $BASE_DOMAIN
+    sh $git_operators/cert-manager/covid19/setup.sh $int_proj $BASE_DOMAIN
+    sh $git_operators/cert-manager/covid19/setup.sh $prod_proj $BASE_DOMAIN
+
+    # install helm chart for covid19 issuer in each project
+    helm upgrade --install vault $charts/covid19-issuer-0.1.0.tgz -n $project_name > /dev/null
+    helm upgrade --install vault $charts/covid19-issuer-0.1.0.tgz -n $dev_proj > /dev/null
+    helm upgrade --install vault $charts/covid19-issuer-0.1.0.tgz -n $int_proj > /dev/null
+    helm upgrade --install vault $charts/covid19-issuer-0.1.0.tgz -n $prod_proj > /dev/null
+
+    # install helm chart for covid19 issuer in each project
+    helm upgrade --install vault $charts/covid19-issuer-0.1.0.tgz -n $project_name --set issuer.create=true > /dev/null
+    helm upgrade --install vault $charts/covid19-issuer-0.1.0.tgz -n $dev_proj --set issuer.create=true > /dev/null
+    helm upgrade --install vault $charts/covid19-issuer-0.1.0.tgz -n $int_proj --set issuer.create=true > /dev/null
+    helm upgrade --install vault $charts/covid19-issuer-0.1.0.tgz -n $prod_proj --set issuer.create=true > /dev/null
 
 }
 
@@ -112,15 +151,18 @@ install_vault() {
         oc new-project cert-manager >/dev/null
     }
 
-    helm repo add hashicorp https://helm.releases.hashicorp.com
-    helm repo update
-    helm upgrade --install vault hashicorp/vault --namespace cert-manager -f $git_operators/vault/override-standalone.yaml
+    helm repo add hashicorp https://helm.releases.hashicorp.com > /dev/null
+    helm repo update > /dev/null
+
+    # replace hard coded hostname in override-standalone.yaml
+    helm upgrade --install vault hashicorp/vault --namespace cert-manager -f $git_operators/vault/override-standalone.yaml --set server.route.host=vault.$BASE_DOMAIN > /dev/null
 
     info "Configuring hashicorp/vault..."
     
     # copy policy.hcl and script to pod
     cp scripts/configure-vault.sh $git_operators/vault/sample/
     oc get pod
+    sleep 10
     oc rsync $git_operators/vault/sample/ vault-0:/tmp/
  
     # execute that script on pod
@@ -134,17 +176,19 @@ install_vault() {
     [ -z $VAULT_TOKEN ] && err "Could not determine VAULT_TOKEN. Exiting. Please rerun again."
 
     info "Vault successfully installed!"
-    info "VAULT_KEY   = $VAULT_KEY"
-    info "VAULT_TOKEN = $VAULT_TOKEN"
+    echo "VAULT_KEY   = $VAULT_KEY"
+    echo "VAULT_TOKEN = $VAULT_TOKEN"
 
 }
 
 
 # Install all the charts and configure the system
 command.install() {
-    oc version >/dev/null 2>&1 || err "no oc binary found"
-    helm version >/dev/null 2>&1 || err "no helm binary found!"
-    openssl version >/dev/null 2>&1 || err "no openssl binary found!"
+    oc version >/dev/null 2>&1 || err "No oc binary found"
+    helm version >/dev/null 2>&1 || err "No helm binary found!"
+    openssl version >/dev/null 2>&1 || err "No openssl binary found!"
+    vault version >/dev/null 2>&1 || err "No vault binary found! Please install from https://vaultproject.io"
+
 
     kube_context=$(oc whoami -c)
     info "Using $kube_context to install datahub..."
@@ -154,6 +198,22 @@ command.install() {
         oc new-project $project_name >/dev/null 
         info "$project_name created"
     }
+
+    oc get ns $dev_proj 2>/dev/null  || { 
+        oc new-project $dev_proj >/dev/null 
+        info "$dev_proj created"
+    }
+
+    oc get ns $int_proj 2>/dev/null  || { 
+        oc new-project $int_proj >/dev/null 
+        info "$int_proj created"
+    }
+
+    oc get ns $prod_proj 2>/dev/null  || { 
+        oc new-project $prod_proj >/dev/null 
+        info "$prod_proj created"
+    }
+
 
     info "Cloning corresponding repositories from GitHub..."
     git version >/dev/null 2>&1 || err "no git binary found"
@@ -190,6 +250,9 @@ command.install() {
     info "Generating Grafana chart..."
     helm package $git_operators/Grafana -u -d $charts
 
+    info "Generating Covid19-Issuer chart..."
+    helm package $git_operators/cert-manager/covid19/helm-charts/covid19-issuer -u -d $charts
+
     # Now install cert-manager
     install_vault
     install_certmanager
@@ -202,9 +265,30 @@ command.uninstall() {
     kube_context=$(oc whoami -c)
     info "Using $kube_context to uninstall datahub..."
 
-    oc delete project cert-manager
-    oc delete project $project_name
+    oc get ns cert-manager 2>/dev/null && { 
+      info "Deleting cert-manager project"
+      oc delete project cert-manager
+    }
 
+    oc get ns $project_name 2>/dev/null && { 
+      info "Deleting $project_name project"
+      oc delete project $project_name
+    }
+
+    oc get ns $dev_proj 2>/dev/null && { 
+      info "Deleting $dev_proj project"
+      oc delete project $dev_proj
+    }
+
+    oc get ns $int_proj 2>/dev/null && { 
+      info "Deleting $int_proj project"
+      oc delete project $int_proj
+    }
+
+    oc get ns $prod_proj 2>/dev/null && { 
+      info "Deleting $prod_proj project"
+      oc delete project $prod_proj
+    }
 }
 
 
